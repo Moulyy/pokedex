d7d1e7d7f9672adac5a469ef0a4dec7d1d226563
Théo Moulart <tmoulart@oslo.fr>
2023-02-02 00:43:39 +0100

    feat(pokemon-form-component.ts): add validation rules to pokemon form

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -1,35 +1,55 @@
 import { PokemonService } from './../services/pokemon.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-form',
   templateUrl: './pokemon-form.component.html',
-  styles: [
-  ]
 })
 export class PokemonFormComponent implements OnInit {
-  pokemonTypes : string[];
-  pokemon: Pokemon;
-
-  constructor(private _pokemonService: PokemonService) {}
+  @Input() pokemon : Pokemon;
+  types : string[];
+  oldPokemon: Pokemon;
+  
+  constructor(private _pokemonService: PokemonService, private _router: Router) {}
 
   ngOnInit(): void {
-    this.pokemonTypes = this._pokemonService.getPokemonTypeList();
+    this.types = this._pokemonService.getPokemonTypeList();
+    this.oldPokemon = {...this.pokemon};
   }
 
   /** Le pokémon en train d'être éditer possède t-il le type passé en paramètre  */
   hasType(type: string) : boolean {
-    return true;
+    return this.pokemon.types.includes(type);
   }
 
   /** Ajoute un type à la liste des types du pokémon ou le retire si le type est déjà présent */
-  selectType() {
+  selectType($event : Event, type: string) {
+    const isChecked : boolean =($event.target as HTMLInputElement).checked;
+    if (isChecked) {
+      this.pokemon.types.push(type);
+    } else {
+      this.pokemon.types = this.pokemon.types.filter(type => type !== type);
+    }
+  }
 
+  /** Empêche un pokemon d'avoir moins de 1 type ou plus de 3 types */
+  isTypesValid(type: string) : boolean {
+    if (this.pokemon.types.length == 1 && this.hasType(type)) {
+      return false;
+    }
+
+    if(this.pokemon.types.length > 2 && !this.hasType(type)) {
+      return false;
+    }
+
+    return true;
   }
 
   /** Soumission du formulaire */
   onSubmit() {
-
+    console.log('Soumission du formulaire');
+    this._router.navigate(['pokemons', this.pokemon.id]);
   }
 }
